4357c849479c2f0b85c58ed961a9c735bfb0e467
rcko07 <107147041+rcko07@users.noreply.github.com>
2022-06-08 22:38:18 +0300

    Update telecopy.py

--- telecopy.py
+++ telecopy.py
@@ -8,12 +8,12 @@ from telethon.tl.types import MessageService
 
 # put your values
 # Telegram API
-API_HASH = "xxx"
-API_ID = "xxx"
+API_HASH = "b2298b31d8ac442a25c3c6d23d8f9b72"
+API_ID = "18885110"
 # Session string by login.py
-SESSION_STRING = "xxx"
-SOURCE_CHAT = '@xxx'
-TARGET_CHAT = '@xxx'
+SESSION_STRING = "BABTkvYx6ccM5TtIP4Apmopk_H-kz1jwSgBYmIhI4cdMQPYQ2tI0kJz8SOPiBPPbxtR7Kg5nqFs7rDFwr7Tgh79HjT_bpShMvGwM5tScarBKMclrpr0A2CKzEr53PYweu0GMkc4vh05lo6EgfXQ_oOy3KKUy5OKI6x5aJ-8NRVFZlluje2A7S6rssIMEiz2cD_D17e2_cOcVvEYLjX02vj1Gzbb6Jx06Gl_vSJ_h1HB51tJaPb4IIzuK5zaX0CV_mOOhnlWi55ZzWSIathFkF1KdaD1J1uzbB4skwWgdIeK-398R7x-KT01bWzlPKucrFxBSSJZjk0vcEOs70e6cqyAAAAAEzgK4EA"
+SOURCE_CHAT = '@sicakindirimn'
+TARGET_CHAT = '@denemea1'
 # Timeout after 50 messages
 LIMIT_TO_WAIT = 50
 
